48d12e09d9737dde12e850ceb8a4023286b37472
jcleigh <cleigh.jordan@gmail.com>
2026-03-09 08:28:28 -0500

    Add mac-mic-mute cask

---
+++ Casks/mac-mic-mute.rb
@@ -0,0 +1,9 @@
+   cask "mac-mic-mute" do
+     version "1.0.0"
+    sha256 "aba4dfbf48f722c1162c8a59149129e672422b74fbcba63c5c6adc70a1ea68aa"
+    url "https://github.com/jcleigh/mac-mic-mute/releases/download/v#{version}/Mac-Mic-Mute.dmg"
+     name "Mac Mic Mute"
+     desc "A simple macOS menu bar app that mutes/unmutes all microphones system-wide."
+     homepage "https://github.com/jcleigh/mac-mic-mute"
+     app "Mac Mic Mute.app"
+   end
